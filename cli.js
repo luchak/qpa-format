a14642ab9bdb6a6ddbc71e613b58db2f655c91cd
luchak <luchak@gmail.com>
2023-08-28 09:59:46 -0700

    Add prettier and lint-staged

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
-import fs from "node:fs/promises";
-import path from "node:path";
-import minimist from "minimist";
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import minimist from 'minimist';
 
 import decodeAudio from 'audio-decode';
-import { encode, decode, QPA_CONFIGS } from "./qpa.js";
-import encodeWAV from "audiobuffer-to-wav";
+import { encode, decode, QPA_CONFIGS } from './qpa.js';
+import encodeWAV from 'audiobuffer-to-wav';
 import waveResampler from 'wave-resampler';
 
 const QPA_SR = 5512;
@@ -13,9 +13,9 @@ const WAV_SR = 22050;
 const argv = minimist(process.argv.slice(2));
 
 if (argv._.length != 2)
-  throw new Error(
-    "usage:\n  node qpa_cli.js [-q <quality>] input.{qpa,wav,mp3,...} output.{qpa,wav}"
-  );
+    throw new Error(
+        'usage:\n  node qpa_cli.js [-q <quality>] input.{qpa,wav,mp3,...} output.{qpa,wav}'
+    );
 
 const input = argv._[0];
 const output = argv._[1];
@@ -23,67 +23,83 @@ const quality = argv.q ?? 2;
 run(input, output);
 
 function arrayToMonoAudioBuffer(array, sampleRate) {
-  return {
-    _channelData: [array],
-    numberOfChannels: 1,
-    sampleRate,
-    getChannelData: function(channel) { return this._channelData[channel] },
-  };
+    return {
+        _channelData: [array],
+        numberOfChannels: 1,
+        sampleRate,
+        getChannelData: function (channel) {
+            return this._channelData[channel];
+        },
+    };
 }
 
 async function run(input, output) {
-  const inFormat = path.extname(input).toLowerCase();
-  const outFormat = path.extname(output).toLowerCase();
+    const inFormat = path.extname(input).toLowerCase();
+    const outFormat = path.extname(output).toLowerCase();
 
-  const outFormats = [".qpa", ".wav"];
+    const outFormats = ['.qpa', '.wav'];
 
-  if (!outFormats.includes(outFormat)) {
-    throw new Error("output format must be .wav or .qpa");
-  }
-
-  const inFile = await fs.readFile(input);
-  let inAudioBuffer, outFile;
-
-  if (inFormat == '.qpa') {
-    const samples = decode(inFile);
-    const audio = arrayToMonoAudioBuffer(new Float32Array(samples.length).fill(0), QPA_SR);
-    for (let i = 0; i < samples.length; i++) {
-      const sample = samples[i] - 128;
-      audio._channelData[0][i] = sample < 0 ? sample / 128 : sample / 127;
-      //audio.channelData[0][i] = (samples[i] - 128) / 127;
+    if (!outFormats.includes(outFormat)) {
+        throw new Error('output format must be .wav or .qpa');
     }
-    inAudioBuffer = audio;
-  } else {
-    inAudioBuffer = await decodeAudio(inFile);
-  }
 
-  let inSamples = new Array(inAudioBuffer.getChannelData(0).length).fill(0);
-  for (let i = 0; i < inAudioBuffer.numberOfChannels; i++) {
-    const channelData = inAudioBuffer.getChannelData(i);
-    for (let j = 0; j < inSamples.length; j++) {
-      inSamples[j] += channelData[j];
-    }
-  }
-  const norm = 1.0 / inAudioBuffer.numberOfChannels;
-  for (let i = 0; i < inSamples.length; i++) {
-    inSamples[i] *= norm;
-  }
+    const inFile = await fs.readFile(input);
+    let inAudioBuffer, outFile;
 
+    if (inFormat == '.qpa') {
+        const samples = decode(inFile);
+        const audio = arrayToMonoAudioBuffer(
+            new Float32Array(samples.length).fill(0),
+            QPA_SR
+        );
+        for (let i = 0; i < samples.length; i++) {
+            const sample = samples[i] - 128;
+            audio._channelData[0][i] = sample < 0 ? sample / 128 : sample / 127;
+            //audio.channelData[0][i] = (samples[i] - 128) / 127;
+        }
+        inAudioBuffer = audio;
+    } else {
+        inAudioBuffer = await decodeAudio(inFile);
+    }
 
-  if (outFormat == '.qpa') {
-    if (inAudioBuffer.sampleRate !== QPA_SR) {
-      inSamples = waveResampler.resample(inSamples, inAudioBuffer.sampleRate, QPA_SR, { method: "sinc" });
+    let inSamples = new Array(inAudioBuffer.getChannelData(0).length).fill(0);
+    for (let i = 0; i < inAudioBuffer.numberOfChannels; i++) {
+        const channelData = inAudioBuffer.getChannelData(i);
+        for (let j = 0; j < inSamples.length; j++) {
+            inSamples[j] += channelData[j];
+        }
     }
-    outFile = encode(inSamples, QPA_CONFIGS['qpa'+quality]);
-  } else {
-    if (inAudioBuffer.sampleRate !== WAV_SR) {
-      // Use point resampling for closest possible approximation to PICO-8 behavior
-      inSamples = waveResampler.resample(inSamples, inAudioBuffer.sampleRate, WAV_SR, { method: "point" });
+    const norm = 1.0 / inAudioBuffer.numberOfChannels;
+    for (let i = 0; i < inSamples.length; i++) {
+        inSamples[i] *= norm;
     }
-    outFile = new Uint8Array(encodeWAV(arrayToMonoAudioBuffer(inSamples, WAV_SR)));
-  }
-  if (outFile) {
-    console.log("Writing:", output);
-    await fs.writeFile(output, outFile);
-  }
+
+    if (outFormat == '.qpa') {
+        if (inAudioBuffer.sampleRate !== QPA_SR) {
+            inSamples = waveResampler.resample(
+                inSamples,
+                inAudioBuffer.sampleRate,
+                QPA_SR,
+                { method: 'sinc' }
+            );
+        }
+        outFile = encode(inSamples, QPA_CONFIGS['qpa' + quality]);
+    } else {
+        if (inAudioBuffer.sampleRate !== WAV_SR) {
+            // Use point resampling for closest possible approximation to PICO-8 behavior
+            inSamples = waveResampler.resample(
+                inSamples,
+                inAudioBuffer.sampleRate,
+                WAV_SR,
+                { method: 'point' }
+            );
+        }
+        outFile = new Uint8Array(
+            encodeWAV(arrayToMonoAudioBuffer(inSamples, WAV_SR))
+        );
+    }
+    if (outFile) {
+        console.log('Writing:', output);
+        await fs.writeFile(output, outFile);
+    }
 }
--- qpa.js
+++ qpa.js
@@ -1,4 +1,4 @@
-import { BitInputStream, BitOutputStream } from "@thi.ng/bitstream";
+import { BitInputStream, BitOutputStream } from '@thi.ng/bitstream';
 
 const QPA1_CONFIG = {
     slice_len: 28,
@@ -9,7 +9,7 @@ const QPA1_CONFIG = {
     chunk_size: 7,
     update_shift: 3,
     predict_shift: 7,
-    magic: 0x31617071, /* 'qpa1' */
+    magic: 0x31617071 /* 'qpa1' */,
 };
 
 const QPA2_CONFIG = {
@@ -17,11 +17,11 @@ const QPA2_CONFIG = {
     scale_bits: 4,
     scale_exponent: 2,
     residual_bits: 2,
-    dequant_tab: [0x10/256, -0x10/256, 0x40/256, -0x40/256],
+    dequant_tab: [0x10 / 256, -0x10 / 256, 0x40 / 256, -0x40 / 256],
     chunk_size: 4,
     update_shift: 3,
     predict_shift: 7,
-    magic: 0x32617071, /* 'qpa2' */
+    magic: 0x32617071 /* 'qpa2' */,
 };
 
 const QPA3_CONFIG = {
@@ -29,11 +29,20 @@ const QPA3_CONFIG = {
     scale_bits: 2,
     scale_exponent: 2,
     residual_bits: 3,
-    dequant_tab: [0x60/256,-0x60/256,0x14b/256,-0x14b/256,0x28f/256,-0x28f/256,0x400/256,-0x400/256],
+    dequant_tab: [
+        0x60 / 256,
+        -0x60 / 256,
+        0x14b / 256,
+        -0x14b / 256,
+        0x28f / 256,
+        -0x28f / 256,
+        0x400 / 256,
+        -0x400 / 256,
+    ],
     chunk_size: 3,
     update_shift: 3,
     predict_shift: 7,
-    magic: 0x33617071, /* 'qpa3' */
+    magic: 0x33617071 /* 'qpa3' */,
 };
 
 const QPA4_CONFIG = {
@@ -41,11 +50,20 @@ const QPA4_CONFIG = {
     scale_bits: 5,
     scale_exponent: 2,
     residual_bits: 3,
-    dequant_tab: [0x2/256,-0x2/256,0x8/256,-0x8/256,0xf/256,-0xf/256,0x18/256,-0x18/256],
+    dequant_tab: [
+        0x2 / 256,
+        -0x2 / 256,
+        0x8 / 256,
+        -0x8 / 256,
+        0xf / 256,
+        -0xf / 256,
+        0x18 / 256,
+        -0x18 / 256,
+    ],
     chunk_size: 3,
     update_shift: 3,
     predict_shift: 7,
-    magic: 0x34617071, /* 'qpa4' */
+    magic: 0x34617071 /* 'qpa4' */,
 };
 
 const QPA5_CONFIG = {
@@ -53,11 +71,28 @@ const QPA5_CONFIG = {
     scale_bits: 4,
     scale_exponent: 2,
     residual_bits: 4,
-    dequant_tab: [0x6/256, -0x6/256, 0x14/256, -0x14/256, 0x24/256, -0x24/256, 0x35/256, -0x35/256, 0x47/256, -0x47/256, 0x5a/256, -0x5a/256, 0x6d/256, -0x6d/256, .5, -.5],
+    dequant_tab: [
+        0x6 / 256,
+        -0x6 / 256,
+        0x14 / 256,
+        -0x14 / 256,
+        0x24 / 256,
+        -0x24 / 256,
+        0x35 / 256,
+        -0x35 / 256,
+        0x47 / 256,
+        -0x47 / 256,
+        0x5a / 256,
+        -0x5a / 256,
+        0x6d / 256,
+        -0x6d / 256,
+        0.5,
+        -0.5,
+    ],
     chunk_size: 2,
     update_shift: 3,
     predict_shift: 7,
-    magic: 0x35617071, /* 'qpa5' */
+    magic: 0x35617071 /* 'qpa5' */,
 };
 
 export const QPA_CONFIGS = {
@@ -66,30 +101,30 @@ export const QPA_CONFIGS = {
     qpa3: QPA3_CONFIG,
     qpa4: QPA4_CONFIG,
     qpa5: QPA5_CONFIG,
-}
+};
 
 function pico_mul(a, b) {
-    return Math.floor((a*b)/65536)&0xffffffff;
+    return Math.floor((a * b) / 65536) & 0xffffffff;
 }
 
 function to_pico(a) {
-    return (a*65536)&(0xffffffff);
+    return (a * 65536) & 0xffffffff;
 }
 
 function from_pico(a) {
-    return a/65536;
+    return a / 65536;
 }
 
 function make_scalefactor_tab(scale_bits, scale_exponent) {
-    return Array(1 << scale_bits).fill().map(
-        (_, s) => Math.pow(s + 1, scale_exponent)
-    );
+    return Array(1 << scale_bits)
+        .fill()
+        .map((_, s) => Math.pow(s + 1, scale_exponent));
 }
 
 function expand_dequant_tab(raw_dequant_tab, scalefactor_tab) {
     return scalefactor_tab.map((sf) => {
         return raw_dequant_tab.map((dq) => {
-            return pico_mul(to_pico(sf),to_pico(dq));
+            return pico_mul(to_pico(sf), to_pico(dq));
         });
     });
 }
@@ -101,10 +136,10 @@ function qpa_clamp(v, min, max) {
 function swap_ends(inbuf) {
     const outbuf = new Uint8Array(inbuf.length);
     for (let i = 0; i < inbuf.length; i += 4) {
-        outbuf[i+0] = inbuf[i+3];
-        outbuf[i+1] = inbuf[i+2];
-        outbuf[i+2] = inbuf[i+1];
-        outbuf[i+3] = inbuf[i+0];
+        outbuf[i + 0] = inbuf[i + 3];
+        outbuf[i + 1] = inbuf[i + 2];
+        outbuf[i + 2] = inbuf[i + 1];
+        outbuf[i + 3] = inbuf[i + 0];
     }
     return outbuf;
 }
@@ -120,27 +155,28 @@ class Encoder {
         this.predict_shift = predict_shift;
         this.update_shift = update_shift;
         this.rms = 0;
-        this.weights[2]=to_pico(-32);
-        this.weights[3]=to_pico(64);
+        this.weights[2] = to_pico(-32);
+        this.weights[3] = to_pico(64);
     }
 
     predict() {
         const weights = this.weights;
         const history = this.history;
         return (
-            pico_mul(weights[0], history[0]) +
-            pico_mul(weights[1], history[1]) +
-            pico_mul(weights[2], history[2]) +
-            pico_mul(weights[3], history[3])
-        )&0xffffffff;
+            (pico_mul(weights[0], history[0]) +
+                pico_mul(weights[1], history[1]) +
+                pico_mul(weights[2], history[2]) +
+                pico_mul(weights[3], history[3])) &
+            0xffffffff
+        );
     }
 
     update(residual, reconstructed) {
         let sample = this.samples[this.idx];
-        sample = sample > 0 ? sample*127 : sample*128;
+        sample = sample > 0 ? sample * 127 : sample * 128;
         const weights = this.weights;
         const history = this.history;
-        const delta = (residual >> this.update_shift);
+        const delta = residual >> this.update_shift;
         weights[0] += history[0] < 0 ? -delta : delta;
         weights[1] += history[1] < 0 ? -delta : delta;
         weights[2] += history[2] < 0 ? -delta : delta;
@@ -149,17 +185,19 @@ class Encoder {
         history[1] = history[2];
         history[2] = history[3];
         history[3] = reconstructed >> this.predict_shift;
-        this.signal_dc = (this.signal_dc + sample)*.5;
-        const rms_sample = sample - .875*this.signal_dc;
-        this.rms += (rms_sample*rms_sample - this.rms)*(1/256);
-        let error = from_pico(to_pico(sample+128)-((reconstructed+0x7f0000)&0xff0000));
+        this.signal_dc = (this.signal_dc + sample) * 0.5;
+        const rms_sample = sample - 0.875 * this.signal_dc;
+        this.rms += (rms_sample * rms_sample - this.rms) * (1 / 256);
+        let error = from_pico(
+            to_pico(sample + 128) - ((reconstructed + 0x7f0000) & 0xff0000)
+        );
 
-        this.error_dc = (this.error_dc + error)*.5;
-        error -= .875*this.error_dc;
+        this.error_dc = (this.error_dc + error) * 0.5;
+        error -= 0.875 * this.error_dc;
 
         this.idx += 1;
 
-        return error*error/(16+this.rms);
+        return (error * error) / (16 + this.rms);
     }
 
     clone_from(other) {
@@ -182,7 +220,11 @@ class Encoder {
     }
 
     copy() {
-        const new_enc = new Encoder(this.samples, this.predict_shift, this.update_shift);
+        const new_enc = new Encoder(
+            this.samples,
+            this.predict_shift,
+            this.update_shift
+        );
         new_enc.clone_from(this);
         return new_enc;
     }
@@ -193,7 +235,9 @@ class Encoder {
 }
 
 export function encode(data, config, err_cb) {
-    const scalefactor_tab = config.scale_tab ?? make_scalefactor_tab(config.scale_bits, config.scale_exponent);
+    const scalefactor_tab =
+        config.scale_tab ??
+        make_scalefactor_tab(config.scale_bits, config.scale_exponent);
     const dequant_tab = expand_dequant_tab(config.dequant_tab, scalefactor_tab);
 
     const num_samples = data.length;
@@ -205,8 +249,7 @@ export function encode(data, config, err_cb) {
     const best_chunk_enc = enc.copy();
 
     const encoded_size =
-      8 /* 8 byte file header */ +
-      num_slices * 4; /* 4 byte slices */
+        8 /* 8 byte file header */ + num_slices * 4; /* 4 byte slices */
 
     // write header
     const stream = new BitOutputStream(encoded_size);
@@ -215,35 +258,55 @@ export function encode(data, config, err_cb) {
     let error = 0;
 
     for (
-      let sample_index = 0;
-      sample_index < num_samples;
-      sample_index += config.slice_len
+        let sample_index = 0;
+        sample_index < num_samples;
+        sample_index += config.slice_len
     ) {
-        const slice_len = qpa_clamp(config.slice_len, 0, num_samples - sample_index);
+        const slice_len = qpa_clamp(
+            config.slice_len,
+            0,
+            num_samples - sample_index
+        );
         let best_sf_error = Infinity;
         let best_sf_slice;
         let best_sf_scale;
 
-        for (let scalefactor = 0; scalefactor < scalefactor_tab.length; scalefactor++) {
+        for (
+            let scalefactor = 0;
+            scalefactor < scalefactor_tab.length;
+            scalefactor++
+        ) {
             const table = dequant_tab[scalefactor];
             sf_enc.clone_from(enc);
             let sf_error = 0;
 
             const slice = [];
-            for (let i = 0; i < slice_len; i+=config.chunk_size) {
+            for (let i = 0; i < slice_len; i += config.chunk_size) {
                 let best_chunk_error = Infinity;
                 let best_chunk_quant;
-                const chunk_size = Math.min(config.chunk_size, slice_len-i);
-                const residual_mask = (1 << config.residual_bits)-1;
-                for (let j = 0; j < (1 << (config.residual_bits * chunk_size)); j++) {
+                const chunk_size = Math.min(config.chunk_size, slice_len - i);
+                const residual_mask = (1 << config.residual_bits) - 1;
+                for (
+                    let j = 0;
+                    j < 1 << (config.residual_bits * chunk_size);
+                    j++
+                ) {
                     chunk_enc.clone_from(sf_enc);
                     let chunk_error = 0;
                     for (let k = 0; k < chunk_size; k++) {
                         const predicted = chunk_enc.predict();
-                        const quant = (j>>(k * config.residual_bits))&residual_mask;
+                        const quant =
+                            (j >> (k * config.residual_bits)) & residual_mask;
                         const dequantized = table[quant];
-                        const reconstructed = qpa_clamp((predicted + dequantized)&0xffffffff, 65536*-128, 65536*127);
-                        chunk_error += chunk_enc.update(dequantized, reconstructed);
+                        const reconstructed = qpa_clamp(
+                            (predicted + dequantized) & 0xffffffff,
+                            65536 * -128,
+                            65536 * 127
+                        );
+                        chunk_error += chunk_enc.update(
+                            dequantized,
+                            reconstructed
+                        );
                         if (chunk_error > best_chunk_error) {
                             break;
                         }
@@ -254,7 +317,7 @@ export function encode(data, config, err_cb) {
                         best_chunk_enc.clone_from(chunk_enc);
                     }
                 }
-                if (best_chunk_error==Infinity) {
+                if (best_chunk_error == Infinity) {
                     throw new Error('no best value found');
                 }
                 sf_error += best_chunk_error;
@@ -263,7 +326,9 @@ export function encode(data, config, err_cb) {
                 }
                 sf_enc.clone_from(best_chunk_enc);
                 for (let k = 0; k < chunk_size; k++) {
-                    const quant = (best_chunk_quant>>(k * config.residual_bits))&residual_mask;
+                    const quant =
+                        (best_chunk_quant >> (k * config.residual_bits)) &
+                        residual_mask;
                     slice.push(quant);
                 }
             }
@@ -275,7 +340,7 @@ export function encode(data, config, err_cb) {
                 best_sf_enc.clone_from(sf_enc);
             }
         }
-        if (best_sf_error==Infinity) {
+        if (best_sf_error == Infinity) {
             throw new Error('no best value found');
         }
         enc.clone_from(best_sf_enc);
@@ -288,7 +353,7 @@ export function encode(data, config, err_cb) {
         error += best_sf_error;
     }
     if (err_cb) {
-        err_cb(Math.sqrt(error/num_samples));
+        err_cb(Math.sqrt(error / num_samples));
     }
 
     return swap_ends(stream.bytes());
@@ -300,44 +365,58 @@ export function decode(bytes, config_override) {
     const num_samples = stream.read(32);
     const samples = new Uint8Array(num_samples);
 
-    const config = config_override ?? Object.values(QPA_CONFIGS).find((c) => c.magic === magic);
+    const config =
+        config_override ??
+        Object.values(QPA_CONFIGS).find((c) => c.magic === magic);
     if (!config) {
         throw new Error('Could not find matching QPA config');
     }
 
-    const scalefactor_tab = config.scale_tab ?? make_scalefactor_tab(config.scale_bits, config.scale_exponent);
+    const scalefactor_tab =
+        config.scale_tab ??
+        make_scalefactor_tab(config.scale_bits, config.scale_exponent);
     const dequant_tab = expand_dequant_tab(config.dequant_tab, scalefactor_tab);
 
-    const enc = new Encoder(new Array(num_samples).fill(0), config.predict_shift, config.update_shift);
+    const enc = new Encoder(
+        new Array(num_samples).fill(0),
+        config.predict_shift,
+        config.update_shift
+    );
 
     for (
         let sample_index = 0;
         sample_index < num_samples;
         sample_index += config.slice_len
     ) {
-          const scalefactor = stream.read(config.scale_bits);
-          const table = dequant_tab[scalefactor];
-          const slice_start = sample_index;
-          const slice_end = Math.min(sample_index + config.slice_len, num_samples);
-          const slice_count = slice_end - slice_start;
-          let idx = slice_start;
-          let bitsRemaining = config.slice_len * config.residual_bits;
-          // note: this loop is a hot code path and could be optimized
-          for (let i = 0; i < slice_count; i++) {
-              const predicted = enc.predict();
-              const quantized = stream.read(config.residual_bits);
-              const dequantized = table[quantized];
-              const reconstructed = qpa_clamp((predicted + dequantized)&0xffffffff, 65536*-128, 65536*127);
-              enc.update(dequantized, reconstructed);
-              samples[idx++] = from_pico(reconstructed) + 128;
-              bitsRemaining -= config.residual_bits;
-          }
-          // skip stream if needed
-          if (bitsRemaining > 0) {
-              stream.read(bitsRemaining);
-          }
+        const scalefactor = stream.read(config.scale_bits);
+        const table = dequant_tab[scalefactor];
+        const slice_start = sample_index;
+        const slice_end = Math.min(
+            sample_index + config.slice_len,
+            num_samples
+        );
+        const slice_count = slice_end - slice_start;
+        let idx = slice_start;
+        let bitsRemaining = config.slice_len * config.residual_bits;
+        // note: this loop is a hot code path and could be optimized
+        for (let i = 0; i < slice_count; i++) {
+            const predicted = enc.predict();
+            const quantized = stream.read(config.residual_bits);
+            const dequantized = table[quantized];
+            const reconstructed = qpa_clamp(
+                (predicted + dequantized) & 0xffffffff,
+                65536 * -128,
+                65536 * 127
+            );
+            enc.update(dequantized, reconstructed);
+            samples[idx++] = from_pico(reconstructed) + 128;
+            bitsRemaining -= config.residual_bits;
+        }
+        // skip stream if needed
+        if (bitsRemaining > 0) {
+            stream.read(bitsRemaining);
+        }
     }
 
     return samples;
 }
-
